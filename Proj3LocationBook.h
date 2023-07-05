a51cdd823bf5badd12c083cc9df485e8b5488617
Bryan Khor <bryankhor07@gmail.com>
2023-07-05 11:20:55 -0700

    Added Proj LocationBook.h to repo

---
+++ Proj3LocationBook.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+#include"Coordinate.h"
+using namespace std;
+
+class LocationBook {
+private:
+	int capacity;
+	int locationCount;
+	double** locations;
+public:
+	LocationBook();
+	void addLocation(Coordinate coordinate);
+	double totalDistanceTravelled();
+	double viewDistanceToAGivenPoint(double x_coordinate, double y_coordinate);
+};
